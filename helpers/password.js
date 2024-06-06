d374a1445060886dcc1b43ee4e3590ae348db98e
Aman Raut <amanraut1111@gmail.com>
2024-06-07 00:38:44 +0530

    add helper function

---
+++ helpers/password.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+
+const hash = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, 10, (err, val) => {
+            resolve(val);
+        });
+    });
+};
+
+const compare = (password1, password2) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password1, password2, (error, value) => {
+            if (error) return reject(error);
+            resolve(value);
+        });
+    });
+};
+
+module.exports = {
+    hash,
+    compare,
+};
