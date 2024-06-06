9b7052f73663eaae04756de3af216c5a8d0b04bc
Aman Raut <amanraut1111@gmail.com>
2024-06-06 23:11:13 +0530

    including admin router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require('./config/db');
 const voterRouter = require("./Routes/voterRouter");
+const adminRouter = require("./Routes/adminRouter");
 require("dotenv").config();
 
 
@@ -9,6 +10,7 @@ app.use(express.json())
 
 
 app.use('/', voterRouter)
+app.use('admin', adminRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("server is Running  on PORT 3000");
