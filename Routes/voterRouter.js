6f61e7db991f2823280cd4cb1d50c001daf4fb9c
Aman Raut <amanraut1111@gmail.com>
2024-06-06 11:34:05 +0530

    add update passowrd route

--- Routes/voterRouter.js
+++ Routes/voterRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerVoter, loginVoter } = require("../controller/voter");
+const { registerVoter, loginVoter, updateVoterProfile, updatePassword, updateVoterPassword } = require("../controller/voter");
 
 const voterRouter = express.Router();
 
 voterRouter.post("/registerCandidate", registerVoter);
 voterRouter.post("/login", loginVoter);
+voterRouter.put('/update/:id', updateVoterProfile)
+voterRouter.patch('/updatePassword/:id', updateVoterPassword)
 
 module.exports = voterRouter;
