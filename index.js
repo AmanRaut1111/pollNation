c1fe48298bf156c63784f57af8cf5339e657200a
Aman Raut <amanraut1111@gmail.com>
2024-06-09 22:38:41 +0530

    add candidtae Router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require('./config/db');
 const voterRouter = require("./Routes/voterRouter");
 const adminRouter = require("./Routes/adminRouter");
+const candidateRouter = require("./Routes/candidateRouter");
 require("dotenv").config();
 
 
@@ -10,7 +11,8 @@ app.use(express.json())
 
 
 app.use('/', voterRouter)
-app.use('admin', adminRouter)
+app.use('/admin', adminRouter)
+app.use('/candidate', candidateRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("server is Running  on PORT 3000");
