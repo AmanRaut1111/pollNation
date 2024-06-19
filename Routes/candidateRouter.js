9821d81594003b760d5af87160abb558e0befc31
Aman Raut <amanraut1111@gmail.com>
2024-06-19 12:46:45 +0530

    add  delete and update Route

--- Routes/candidateRouter.js
+++ Routes/candidateRouter.js
@@ -1,9 +1,11 @@
 
 const express = require('express')
-const { addCandidate, getAllCandidate } = require('../controller/candidate')
+const { addCandidate, getAllCandidate, deleteCandidate, updateCandidate } = require('../controller/candidate')
 const candidateRouter = express.Router()
 
 candidateRouter.post("/add", addCandidate)
 candidateRouter.get('/getDetails', getAllCandidate)
+candidateRouter.delete('/delete/:id', deleteCandidate)
+candidateRouter.put('/update/:id', updateCandidate)
 
 module.exports = candidateRouter
