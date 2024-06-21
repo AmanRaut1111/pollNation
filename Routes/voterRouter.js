d96ad4eaeed38881522552d83d41c7006db3c5d6
Aman Raut <amanraut1111@gmail.com>
2024-06-21 23:20:23 +0530

    change in lin34

--- Routes/voterRouter.js
+++ Routes/voterRouter.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { registerVoter, loginVoter, updateVoterProfile, updateVoterPassword, getVoterDetails, giveVote } = require("../controller/voter");
+const {
+    registerVoter,
+    loginVoter,
+    updateVoterProfile,
+    updateVoterPassword,
+    getVoterDetails,
+    giveVote,
+} = require("../controller/voter");
 const verifyToken = require("../auth");
 
 const voterRouter = express.Router();
 
 voterRouter.post("/registerVoter", registerVoter);
 voterRouter.post("/login", verifyToken, loginVoter);
-voterRouter.put('/update/:id', updateVoterProfile)
-voterRouter.patch('/updatePassword/:id', updateVoterPassword)
-voterRouter.get('/getVoterDetails', getVoterDetails)
-voterRouter.post('/giveVote/:id', giveVote)
+voterRouter.put("/update/:id", updateVoterProfile);
+voterRouter.patch("/updatePassword/:id", updateVoterPassword);
+voterRouter.get("/getVoterDetails", getVoterDetails);
+voterRouter.post("/giveVote/:id", giveVote);
 
 module.exports = voterRouter;
