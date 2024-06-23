37719df55f63630dec3faf535a79994be416ffbd
Aman Raut <amanraut1111@gmail.com>
2024-06-23 11:39:22 +0530

    change in line 12

--- Routes/candidateRouter.js
+++ Routes/candidateRouter.js
@@ -1,11 +1,14 @@
 
+
 const express = require('express')
-const { addCandidate, getAllCandidate, deleteCandidate, updateCandidate } = require('../controller/candidate')
+const { addCandidate, getAllCandidate, deleteCandidate, updateCandidate, getAllCandidateFromState, getCandidateWithParty } = require('../controller/candidate')
 const candidateRouter = express.Router()
 
 candidateRouter.post("/add", addCandidate)
 candidateRouter.get('/getDetails', getAllCandidate)
 candidateRouter.delete('/delete/:id', deleteCandidate)
 candidateRouter.put('/update/:id', updateCandidate)
+candidateRouter.get('/fromState', getAllCandidateFromState)
+candidateRouter.get('/fromParty', getCandidateWithParty)
 
 module.exports = candidateRouter
