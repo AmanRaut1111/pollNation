30f93db3a89d88b7e575c1109113c1233d32b09d
Aman Raut <amanraut1111@gmail.com>
2024-06-24 18:26:23 +0530

    add serach functionality in getvoterDetails

--- Routes/candidateRouter.js
+++ Routes/candidateRouter.js
@@ -1,7 +1,7 @@
 
 
 const express = require('express')
-const { addCandidate, getAllCandidate, deleteCandidate, updateCandidate, getAllCandidateFromState, getCandidateWithParty, countVote, winnerCandidate } = require('../controller/candidate')
+const { addCandidate, getAllCandidate, deleteCandidate, updateCandidate, getAllCandidateFromState, getCandidateWithParty, countVote, winnerCandidate, winnerFromState } = require('../controller/candidate')
 const candidateRouter = express.Router()
 
 candidateRouter.post("/add", addCandidate)
@@ -12,5 +12,6 @@ candidateRouter.get('/fromState', getAllCandidateFromState)
 candidateRouter.get('/fromParty', getCandidateWithParty)
 candidateRouter.get('/countVote', countVote)
 candidateRouter.get('/winnerCandidate', winnerCandidate)
+candidateRouter.get('/winnerFromstate', winnerFromState)
 
 module.exports = candidateRouter
