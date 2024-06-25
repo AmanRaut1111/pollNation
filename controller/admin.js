09e8c1a9f1ddbd3d8b3131394e89487d4968e712
Aman Raut <amanraut1111@gmail.com>
2024-06-25 16:00:26 +0530

    add admin logout

--- controller/admin.js
+++ controller/admin.js
@@ -45,51 +45,83 @@ const loginAdmin = async (req, res) => {
                     password,
                     check.password
                 );
+
+                req.session.adminId = check._id;
+
                 if (checkPassword) {
-                    res
-                        .status(200)
-                        .json({
-                            message: "Login Sucessfully...!",
-                            status: true,
-                            statusCode: 200,
-                        });
+                    res.status(200).json({
+                        message: "Login Sucessfully...!",
+                        status: true,
+                        statusCode: 200,
+                    });
                 } else {
-                    res
-                        .statsu(400)
-                        .json({
-                            message: "Password Does Not Match ..Please Try Agin",
-                            status: false,
-                            statusCode: 400,
-                        });
-                }
-            } else {
-                res
-                    .status(400)
-                    .json({
-                        message: "Admin Not Found....!",
+                    res.statsu(400).json({
+                        message: "Password Does Not Match ..Please Try Agin",
                         status: false,
                         statusCode: 400,
                     });
-            }
-        } else {
-            res
-                .status(400)
-                .json({
-                    message: "Something Went Wrong..!",
-                    status: true,
+                }
+            } else {
+                res.status(400).json({
+                    message: "Admin Not Found....!",
+                    status: false,
                     statusCode: 400,
                 });
+            }
+        } else {
+            res.status(400).json({
+                message: "Something Went Wrong..!",
+                status: true,
+                statusCode: 400,
+            });
         }
     } catch (error) {
         console.log(error);
-        res
-            .status(500)
-            .json({
-                message: "Something Went Wrong..!",
-                status: true,
-                statusCode: 500,
-            });
+        res.status(500).json({
+            message: "Something Went Wrong..!",
+            status: true,
+            statusCode: 500,
+        });
     }
 };
 
-module.exports = { registerAdmin, loginAdmin };
+const adminLogout = (req, res) => {
+    try {
+        console.log("Cookies:", req.cookies); // Log cookies
+        if (req.session.adminId) {
+            req.session.destroy((err) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: "LogOut failed...!",
+                        status: false,
+                        stastsuCode: 500,
+                    });
+                }
+            });
+
+            res.clearCookie("connect.sid"); // Clear the session cookie
+            res
+                .status(200)
+                .json({
+                    message: "Logout sucessfully..!",
+                    status: true,
+                    statusCode: 200,
+                });
+        } else {
+            res.status(400).json({
+                message: "No Admin is Logged In.",
+                status: false,
+                statusCode: 400,
+            });
+        }
+    } catch (error) {
+        console.error("Unexpected error during logout:", error);
+        res.status(500).json({
+            message: "Something Went Wrong During Logout.",
+            status: false,
+            statusCode: 500,
+        });
+    }
+};
+
+module.exports = { registerAdmin, loginAdmin, adminLogout };
