3225730733f1ecad5f92ad4639a5badd175a31d4
Aman Raut <amanraut1111@gmail.com>
2024-06-27 14:26:47 +0530

    update candite statsu route

--- Routes/candidateRouter.js
+++ Routes/candidateRouter.js
@@ -1,17 +1,27 @@
+const express = require("express");
+const {
+    addCandidate,
+    getAllCandidate,
+    deleteCandidate,
+    updateCandidate,
+    getAllCandidateFromState,
+    getCandidateWithParty,
+    countVote,
+    winnerCandidate,
+    winnerFromState,
+    updateWinnerStatus,
+} = require("../controller/candidate");
+const candidateRouter = express.Router();
 
+candidateRouter.post("/add", addCandidate);
+candidateRouter.get("/getDetails", getAllCandidate);
+candidateRouter.delete("/delete/:id", deleteCandidate);
+candidateRouter.put("/update/:id", updateCandidate);
+candidateRouter.get("/fromState", getAllCandidateFromState);
+candidateRouter.get("/fromParty", getCandidateWithParty);
+candidateRouter.get("/countVote", countVote);
+candidateRouter.get("/winnerCandidate", winnerCandidate);
+candidateRouter.get("/winnerFromstate", winnerFromState);
+candidateRouter.patch("/updatestatus/:id", updateWinnerStatus);
 
-const express = require('express')
-const { addCandidate, getAllCandidate, deleteCandidate, updateCandidate, getAllCandidateFromState, getCandidateWithParty, countVote, winnerCandidate, winnerFromState } = require('../controller/candidate')
-const candidateRouter = express.Router()
-
-candidateRouter.post("/add", addCandidate)
-candidateRouter.get('/getDetails', getAllCandidate)
-candidateRouter.delete('/delete/:id', deleteCandidate)
-candidateRouter.put('/update/:id', updateCandidate)
-candidateRouter.get('/fromState', getAllCandidateFromState)
-candidateRouter.get('/fromParty', getCandidateWithParty)
-candidateRouter.get('/countVote', countVote)
-candidateRouter.get('/winnerCandidate', winnerCandidate)
-candidateRouter.get('/winnerFromstate', winnerFromState)
-
-module.exports = candidateRouter
+module.exports = candidateRouter;
